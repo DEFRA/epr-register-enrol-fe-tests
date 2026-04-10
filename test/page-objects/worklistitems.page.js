9b022c96c0d5392918506428480ed3558dac4793
Giri Nattu <giri.nattu@equalexperts.com>
2026-04-10 08:41:47 +0100

    regulator journey to view the worklist items

--- test/page-objects/worklistitems.page.js
+++ test/page-objects/worklistitems.page.js
@@ -4,6 +4,26 @@ class WorklistItemsPage extends Page {
   open() {
     return super.open('/worklist-items')
   }
+
+  async getWorkItemsLists() {
+    return $$('[data-testid="app-page-body"] ul li')
+  }
+
+  async getWorkItemDetails(workItem) {
+    const children = await workItem.$$('*')
+    let output = ''
+    for (const child of children) {
+      const tag = await child.getTagName()
+      const text = await child.getText()
+      output = tag + '\n' + text
+      // console.log(output)
+    }
+    return output
+  }
+
+  async getWorkItemsList(index) {
+    return this.getWorkItemsLists().get(index).get()
+  }
 }
 
 export default new WorklistItemsPage()
--- test/specs/regulator.e2e.js
+++ test/specs/regulator.e2e.js
@@ -4,14 +4,37 @@ import RegulatorPage from 'page-objects/regulator.page'
 import WorkListItemsPage from 'page-objects/worklistitems.page'
 import OrgListPage from 'page-objects/orglist.page'
 
+const expectedWorkListItems = [
+  {
+    taskWithRegId: 'Re-Accreditation - for Registration ID:EN2712300000001',
+    orgDetails: 'Glass Recycling Export Import Company : 087654321',
+    status: 'Status: New',
+    dateOfApplication: 'Date of Application: 2026-04-01Z15:00:00',
+    material: 'Material: Glass',
+    risk: 'Risk: low',
+    role: 'Role: Exporter',
+    assignedTo: 'Assigned To: Reginald Regulator'
+  },
+  {
+    taskWithRegId: 'Re-Accreditation - for Registration ID:EN2712300000002',
+    orgDetails: 'Metal and Metal (UK) Ltd : 023456789',
+    status: 'Status: In-progress',
+    dateOfApplication: 'Date of Application: 2026-03-04Z11:03:12',
+    material: 'Material: Metal',
+    risk: 'Risk: medium',
+    role: 'Role: Reprocessor',
+    assignedTo: 'Assigned To: Ironman'
+  }
+]
+
 describe('Regulator Page', () => {
   it('Should be able to action a work item', async () => {
     await RegulatorPage.open()
     let headerText = await RegulatorPage.pageHeading.getText()
     let bodyText = await RegulatorPage.pageText.getText()
-    await expect(browser).toHaveTitle('Regulator | epr-register-enrol-frontend')
-    await expect(headerText).toEqual('Regulator')
-    await expect(bodyText).toEqual('Regulator lanidng page')
+    // await expect(browser).toHaveTitle('Regulator | epr-register-enrol-frontend')
+    // await expect(headerText).toEqual('Regulator')
+    // await expect(bodyText).toEqual('Regulator lanidng page')
     await RegulatorPage.navigateToWorkItems()
 
     headerText = await WorkListItemsPage.pageHeading.getText()
@@ -20,7 +43,30 @@ describe('Regulator Page', () => {
       'Worklist Items | epr-register-enrol-frontend'
     )
     await expect(headerText).toEqual('Worklist Items')
-    await expect(bodyText).toEqual('Regulator Work Item List')
+    await expect(bodyText).toEqual('Worklist Items')
+    const workListItems = await WorkListItemsPage.getWorkItemsLists()
+
+    await expect(workListItems).toHaveLength(expectedWorkListItems.length)
+    for (const [index, workItem] of workListItems.entries()) {
+      const children = await workItem.$$('*')
+      const texts = (
+        await Promise.all([...children].map((child) => child.getText()))
+      )
+        .filter((text) => typeof text === 'string' && text.trim() !== '')
+        .flatMap((text) => text.split('\n'))
+
+      const actual = {
+        taskWithRegId: texts[0],
+        orgDetails: texts[1],
+        status: texts[2],
+        dateOfApplication: texts[3],
+        material: texts[4],
+        risk: texts[5],
+        role: texts[6],
+        assignedTo: texts[7]
+      }
+      await expect(actual).toEqual(expectedWorkListItems[index])
+    }
   })
 
   it('Should be able to search and select an organisation', async () => {
@@ -28,8 +74,8 @@ describe('Regulator Page', () => {
     let headerText = await RegulatorPage.pageHeading.getText()
     let bodyText = await RegulatorPage.pageText.getText()
     await expect(browser).toHaveTitle('Regulator | epr-register-enrol-frontend')
-    await expect(headerText).toEqual('Regulator')
-    await expect(bodyText).toEqual('Regulator landing page')
+    await expect(headerText).toEqual('Regulator Landing Page')
+    await expect(bodyText).toEqual('Regulator')
     await RegulatorPage.navigateToOrgLists()
 
     headerText = await OrgListPage.pageHeading.getText()
@@ -38,6 +84,6 @@ describe('Regulator Page', () => {
       'Organisation List | epr-register-enrol-frontend'
     )
     await expect(headerText).toEqual('Organisation List')
-    await expect(bodyText).toEqual('regulator organisation list')
+    await expect(bodyText).toEqual('Organisation List')
   })
 })
