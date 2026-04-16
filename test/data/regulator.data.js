63013c561d014b3a5a4c1d53358bcb76a9238a3b
Giri Nattu <giri.nattu@equalexperts.com>
2026-04-16 10:47:47 +0100

    fix failing tests, add missing file

---
+++ test/data/regulator.data.js
@@ -0,0 +1,32 @@
+export const expectedWorkListItems = [
+  {
+    taskWithRegId: 'Re-Accreditation for Registration ID:EN2712300000001',
+    orgDetails: 'Glass Recycling Export Import Company : 087654321',
+    status: 'Status: New',
+    dateOfApplication: 'Date of Application: 2026-04-01Z15:00:00',
+    material: 'Material: Glass',
+    risk: 'Risk: low',
+    role: 'Role: Exporter',
+    assignedTo: 'Assigned To: Reginald Regulator'
+  },
+  {
+    taskWithRegId: 'Re-Accreditation for Registration ID:EN2712300000002',
+    orgDetails: 'Metal and Metal (UK) Ltd : 023456789',
+    status: 'Status: In-progress',
+    dateOfApplication: 'Date of Application: 2026-03-04Z11:03:12',
+    material: 'Material: Metal',
+    risk: 'Risk: medium',
+    role: 'Role: Reprocessor',
+    assignedTo: 'Assigned To: Ironman'
+  }
+]
+
+export const expectedOrgSummary = {
+  'Company name': 'Bananaman Export Company',
+  'Registration ID': 'BN2712300000001',
+  'Companies house number': '07620513',
+  'Registered address': '29 Acacia Road',
+  'Approved person': 'General Blight',
+  Directors: 'Eric Twinge\nCrow\nDoctor Gloom',
+  'Org Status': 'Active'
+}
