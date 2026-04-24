87f4df298b6821a5bc3f9ad9a3d1d0fea18914e1
Giri Nattu <giri.nattu@equalexperts.com>
2026-04-24 08:38:14 +0100

    updated to get the api url in ci

--- test/config.js
+++ test/config.js
@@ -1,1 +1,7 @@
-export const backendUrl = process.env.API_BASE_URL || 'http://localhost:8080'
+const environment = process.env.ENVIRONMENT
+
+export const backendUrl =
+  process.env.API_BASE_URL ||
+  (environment
+    ? `https://epr-register-enrol-backend.${environment}.cdp-int.defra.cloud`
+    : 'http://localhost:8080')
